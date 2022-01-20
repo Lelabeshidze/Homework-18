2e6487ca5b4099a8774b00dc9a57182948f66d13
Lelabeshidze <lela.beshidze@gmail.com>
2022-01-20 16:48:41 +0400

    added slideshow

--- function.js
+++ function.js
@@ -19,3 +19,65 @@ function startTime() {
   
   
   startTime()
+
+//   დავალევა2
+
+  let slideIndex = 1;
+  let millis = 1000;
+  let interval;
+  
+  startSlides();
+  
+  function startSlides(){
+    pauseSlides();
+    nextSlide();
+    interval = setInterval(nextSlide, millis);
+  }
+
+
+  function resumeSlides() {
+    nextSlide();
+  }
+
+  function pauseSlides() {
+    clearInterval(interval);
+  }
+
+  function nextSlide() {
+    showSlide();
+    slideIndex++;
+  }
+
+  function plusSlides(n) {
+    clearInterval(interval);
+    slideIndex += n;
+    nextSlide();
+    interval = setInterval(nextSlide, millis);
+  }
+
+  function currentSlide(n) {
+    clearInterval(interval);
+    slideIndex = n + 1;
+    nextSlide();
+    interval = setInterval(nextSlide, millis);
+  }
+
+  function showSlide() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
